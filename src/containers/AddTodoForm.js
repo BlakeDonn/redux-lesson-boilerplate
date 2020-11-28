5162cadebaee6f3b91215990100904789e170f02
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-28 15:17:07 -0500

    Adjust state management

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -3,7 +3,13 @@ import { addToDo } from '../actions';
 import { connect } from 'react-redux';
 
 class AddTodoForm extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = { todo: '' };
+  }
+  handleChange = (e) => {
+    this.setState({ todo: e.target.value });
+  }
   submitForm = (e) => {
     e.preventDefault()
     this.props.addToDo(this.state.todo)
