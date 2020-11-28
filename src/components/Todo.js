fac3cb723b4bd864c797d2101db6d7380ee40236
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-28 16:48:41 -0500

    Add in functionality to mapDispatchToProps

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { completeTask } from "../actions";
+import { connect } from 'react-redux';
 
-const ToDo = ({ id, todo, completed }) => {
-  return <li>{todo}</li>;
+const ToDo = ({ completeTask, id, todo, completed }) => {
+  return (
+    <li
+      className={completed ? "completed" : "not-completed"}
+      onClick={()=> completeTask(id, todo)}
+    >
+      {todo}
+    </li>
+  );
 };
-export default ToDo;
+const mapDispatchToProps = dispatch => ({
+  completeTask: (id, todo) => dispatch( completeTask(id, todo) )
+})
+
+export default connect(null, mapDispatchToProps)(ToDo);
+
